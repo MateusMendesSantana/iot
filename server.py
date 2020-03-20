713c9958d58174a8c4569f4b23bf36f8b2164888
Mateus Mendes <mateusmendessantana@hotmail.com>
2020-03-19 21:33:23 -0300

    Atividade 1

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+HOST = ''
+PORT = 5000
+
+def conectado(con, cliente):
+  print('Conectado por', cliente)
+
+  while True:
+    msg = con.recv(1024)
+
+    if not msg:
+      break
+
+    print(cliente, msg)
+
+  print('Finalizando conexao do cliente', cliente)
+  con.close()
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+orig = (HOST, PORT)
+
+tcp.bind(orig)
+tcp.listen(1)
+
+print('Ouvindo conexões ;D')
+
+while True:
+  con, cliente = tcp.accept()
+  threading._start_new_thread(conectado, tuple([con, cliente]))
+
+tcp.close()
